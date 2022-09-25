931aabc6ebff15209f934f6c533c4579e99f2761
Sabri <nooky77@users.noreply.github.com>
2022-09-26 01:58:18 +0200

    Update disemvowel.js
    Fix Typos

--- 7kyu/disemvowel.js
+++ 7kyu/disemvowel.js
@@ -24,9 +24,5 @@ function disemvowel(str) {
 }
 
 console.log(disemvowel("This website is for losers LOL!")); // "Ths wbst s fr lsrs LL!")
-console.log(
-	disemvowel(
-		"No offense but,\nYour writing is among the worst I've ever read"
-	)
-); // "N ffns bt,\nYr wrtng s mng th wrst 'v vr rd")
+console.log(disemvowel("No offense but,\nYour writing is among the worst I've ever read")); // "N ffns bt,\nYr wrtng s mng th wrst 'v vr rd")
 console.log(disemvowel("What are you, a communist?")); // "Wht r y,  cmmnst?")
