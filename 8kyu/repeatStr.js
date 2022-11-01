4dfa5b97d268cdeb04ee85267f80d8bcf810a520
Youcef Sabri <rsg951@gmail.com>
2022-11-01 01:45:31 +0100

    8kyu repeatStr

---
+++ 8kyu/repeatStr.js
@@ -0,0 +1,14 @@
+// Write a function that accepts an integer n and a string s as parameters,
+// and returns a string of s repeated exactly n times.
+
+function repeatStr(n, s) {
+	let result = "";
+	for (let i = 0; i < n; i++) {
+		result += s;
+	}
+	return result;
+}
+
+console.log(repeatStr(3, "*"), "***");
+console.log(repeatStr(5, "#"), "#####");
+console.log(repeatStr(2, "ha "), "ha ha ");
